88ea612b8f6c564b7eb2ad0ab8476fa85d9d503c
webnerd15 <bbk@webnerdtz.com>
2023-11-17 12:05:52 +0000

    updated the database connection variable

--- db.py
+++ db.py
@@ -1,18 +1,18 @@
 from sqlalchemy import create_engine, text
 import os
 
-connection_string = os.environ['DB_CONNECTION_STRING']
-engine = create_engine(connection_string)
-connection = engine.connect()
+engine = create_engine(os.environ['DB_CONNECTION_STRING'])
+#dbc -> database_connection object
+dbc = engine.connect()
 
-#connection.execute(text("CREATE TABLE IF NOT EXISTS example(id INTEGER, name VARCHAR(20))"))
+#dbc.execute(text("CREATE TABLE IF NOT EXISTS example(id INTEGER, name VARCHAR(20))"))
 
-#connection.execute(text("INSERT INTO example (name) VALUES (:name)"), {"name": "Ashley"})
-#connection.execute(text("INSERT INTO example (name) VALUES (:name)"), [{"name": "Barry"}, {"name": "Christina"}])
-#connection.commit()
-sql = "SELECT bible_kjv.text, bible_books_en.fullname, bible_kjv.chapter, bible_kjv.verse FROM bible_kjv LEFT JOIN bible_books_en ON bible_kjv.book = bible_books_en.number WHERE bible_kjv.book = 40 AND bible_kjv.chapter = 1 AND bible_kjv.verse= 1"
+#dbc.execute(text("INSERT INTO example (name) VALUES (:name)"), {"name": "Ashley"})
+#dbc.execute(text("INSERT INTO example (name) VALUES (:name)"), [{"name": "Barry"}, {"name": "Christina"}])
+#dbc.commit()
+sql = "SELECT bible_kjv.text, bible_books_en.fullname, bible_kjv.chapter, bible_kjv.verse FROM bible_kjv LEFT JOIN bible_books_en ON bible_kjv.book = bible_books_en.number WHERE bible_kjv.book = 41 AND bible_kjv.chapter = 1 AND bible_kjv.verse= 1"
 
-result = connection.execute(text(sql))
+result = dbc.execute(text(sql))
 
 for row in result.mappings():
   #print(row) 
