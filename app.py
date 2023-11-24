b2d50b539fb57b25418aaa5743841a263a9b5143
webnerd15 <bbk@webnerdtz.com>
2023-11-24 06:38:21 +0000

    Added functionality of pulling random bible verses and return as json

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import magma
+import db, random
 
 app = Flask(__name__)
 
@@ -17,17 +17,20 @@ def home():
   return "Biblia Leo Verson 0.1"
 
 
-@app.route("/api/books/")
-def bible_books():
-  return jsonify(BOOKS)
+@app.route("/api/<book>/<chapter>/<verse>")
+def bible_books(book,chapter,verse):
+  return jsonify(db.fetch_a_verse(book,chapter,verse))
 
-@app.route("/cheka")
-def lough():
-  return magma.cheka()
-
-@app.route("/lia")
-def cry():
-  return magma.lia()
+@app.route("/api/en/verselinestoday/<lines>")
+def verse_today(lines):
+  if(int(lines)>=1):
+    return jsonify(db.get_random_verse(int(lines)))
+  else:
+    return jsonify(db.get_random_verse(random.randint(1,10)))
+  
+@app.route("/api/en/versetoday/")
+def verse_today_free():
+    return jsonify(db.get_random_verse(random.randint(1,10)))
   
 if __name__ == "__main__":
   app.run("0.0.0.0", debug=True)
--- db.py
+++ db.py
@@ -1,19 +1,93 @@
 from sqlalchemy import create_engine, text
-import os
+import os, random
 
-engine = create_engine(os.environ['DB_CONNECTION_STRING'])
+engine = create_engine(
+  os.environ['DB_CONNECTION_STRING'],
+  connect_args={
+    "ssl": {
+      "ssl_ca":"/etc/ssl/cert.pem"
+    }
+  })
 #dbc -> database_connection object
-dbc = engine.connect()
+#dbc = engine.connect()
 
 #dbc.execute(text("CREATE TABLE IF NOT EXISTS example(id INTEGER, name VARCHAR(20))"))
 
 #dbc.execute(text("INSERT INTO example (name) VALUES (:name)"), {"name": "Ashley"})
 #dbc.execute(text("INSERT INTO example (name) VALUES (:name)"), [{"name": "Barry"}, {"name": "Christina"}])
 #dbc.commit()
-sql = "SELECT bible_kjv.text, bible_books_en.fullname, bible_kjv.chapter, bible_kjv.verse FROM bible_kjv LEFT JOIN bible_books_en ON bible_kjv.book = bible_books_en.number WHERE bible_kjv.book = 41 AND bible_kjv.chapter = 1 AND bible_kjv.verse= 1"
 
-result = dbc.execute(text(sql))
+#WHERE bible_kjv.book = :book AND bible_kjv.chapter = :chapter AND bible_kjv.verse= :verse"
+'''
+with engine.connect() as dbc:
+  sql = "SELECT bible_kjv.text, bible_books_en.fullname, bible_kjv.chapter, bible_kjv.verse FROM bible_kjv LEFT JOIN bible_books_en ON bible_kjv.book = bible_books_en.number LIMIT 0,5"
+  
+  result = dbc.execute(text(sql))
 
-for row in result.mappings():
-  #print(row) 
-  print("Verse:" , row["text"], row['fullname'], row['chapter'])
+  res_dict = []
+  for row in result.all():
+    print(row.fullname, row.chapter, row.verse, row.text)
+    res_dict.append([{"Title":row.fullname, "Chapter":row.chapter, "Verse":row.verse, "Text":row.text
+                     }])
+    #res_dict.append(dict(row))
+  print("===========================")
+  print(res_dict)
+  dbc.close()
+'''
+
+sql = "SELECT chapters FROM bible_books_en WHERE number = :book"
+with engine.connect() as dbc:
+  result = dbc.execute(text(sql),dict(book=2))
+  chapters = result.first()[0]
+  dbc.close()
+  print(chapters)
+
+def fetch_a_verse(book, chapter, verse):
+  sql = "SELECT bible_kjv.text, bible_books_en.fullname, bible_kjv.chapter, bible_kjv.verse FROM bible_kjv LEFT JOIN bible_books_en ON bible_kjv.book = bible_books_en.number WHERE bible_kjv.book = :book AND bible_kjv.chapter = :chapter AND bible_kjv.verse= :verse"
+  
+  with engine.connect() as dbc:
+    result = dbc.execute(text(sql),dict(book=book,chapter=chapter,verse=verse))
+    res_dict = []
+    for row in result.all():
+      res_dict.append([{"Title":row.fullname, "Chapter":row.chapter, "Verse":row.verse, "Text":row.text}])
+    dbc.close()
+    
+    return res_dict
+
+def get_number_of_chapters(book):
+  sql = "SELECT chapters FROM bible_books_en WHERE number = :book"
+  with engine.connect() as dbc:
+    result = dbc.execute(text(sql),dict(book=book))
+    return result.first()[0]
+
+def get_number_of_verses(book, chapter):
+  sql = "SELECT COUNT(verse) AS verses FROM bible_kjv WHERE book = :book AND chapter = :chapter"
+  with engine.connect() as dbc:
+    result = dbc.execute(text(sql),dict(book=book,chapter=chapter))
+    return result.first()[0]
+
+def get_random_verse(number_of_lines):
+  # get a random book from 66 books availble
+  book = random.randint(1,66)
+  # get number of chapters from the random chosen book
+  chapters = get_number_of_chapters(book)
+  # get a random chapter from the available chapters
+  random_chapter = random.randint(1,chapters)
+  # get number of verses from the random chosen chapter
+  verses = get_number_of_verses(book, random_chapter)
+  # get a random verse to start from the available verses
+  start_verse = random.randint(1,verses)
+  
+  sql = "SELECT bible_kjv.text, bible_books_en.fullname, bible_kjv.chapter, bible_kjv.verse FROM bible_kjv LEFT JOIN bible_books_en ON bible_kjv.book = bible_books_en.number WHERE bible_books_en.number = :book AND bible_kjv.chapter = :chapter LIMIT :start_verse,:number_of_lines"
+  
+  with engine.connect() as dbc:
+    result = dbc.execute(text(sql), dict(book=book,chapter=random_chapter,start_verse=start_verse,number_of_lines=number_of_lines))
+    
+    res_dict = []
+    for row in result.all():
+      res_dict.append([{"Title":row.fullname, "Chapter":row.chapter, "Verse":row.verse, "Text":row.text}])
+    
+    dbc.close()
+    
+    return res_dict
+    
